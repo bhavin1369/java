5d5c0d7cbcdad1743902a203a5f5680b74c38a5b
bhavin1369 <138358612+bhavin1369@users.noreply.github.com>
2024-09-16 14:33:56 +0530

    Add files via upload

---
+++ circle.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class circle {
+    public static void main(String[] args) {
+        int r,c;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter value r:");
+        r=sc.nextInt();
+        System.out.println("the area of circle is :"+(3.14*r*r));
+}
+}
---
+++ circleperamiter.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class circleperamiter {
+    public static void main(String[] args) {
+        int r;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter value r:");
+        r=sc.nextInt();
+        System.out.println("the peramiter of circle is :"+(2*3.14*r));
+}
+}
