cc8f6db513833ac24ed2d274b41ebd6c00221b6f
bhavin1369 <138358612+bhavin1369@users.noreply.github.com>
2024-10-28 10:58:50 +0530

    Add files via upload

---
+++ square.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class square {
+    public static void main(String[] args) {
+        int a;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter value a:");
+        a=sc.nextInt();
+        System.out.println("the area of square  is :"+(a*a));
+}
+    
+}
---
+++ squareperamiter.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class squareperamiter {
+    public static void main(String[] args) {
+        int a;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter value a:");
+        a=sc.nextInt();
+        System.out.println("the peramiter of square  is :"+(4*a));
+}
+    
+}
